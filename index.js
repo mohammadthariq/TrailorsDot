8b5156030cde03680c207e3357b14f28ca5b886b
mohammadthariq <thariq.fswd@gmail.com>
2023-03-10 10:01:03 +0530

    mongoose connect

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("Hello World");
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const app = express();
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://shenbasv98:TrailorsDot@trailorsdot.5kfvhrr.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+client.on("error", (err) => {
+  console.log(err);
+});
+client.once("open", () => {
+  console.log("success");
+});
+
+app.get("/", async (req, res) => {
+  res.send("thariq");
+});
+
+// const Port = process.env.PORT || 8080;
+
+app.listen(8080, () => console.log(`Server is Running Successfully on 8080`));
